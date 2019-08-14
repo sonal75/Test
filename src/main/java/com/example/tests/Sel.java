ab959f616ff2db6b5b78d413c02ae37288614ec1
sonal75 <51466612+sonal75@users.noreply.github.com>
2019-08-14 15:39:47 -0500

    commit

---
+++ src/main/java/com/example/tests/Sel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.tests;
+
+/**
+ *
+ * @author silks
+ */
+public class Sel {
+    public int add(int a, int b){
+        if (a>100){
+            return a+b;
+        }
+    return a+b;
+}
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    
+}
